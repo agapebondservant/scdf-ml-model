d19977a68f230f871b61725aa4fe45d45999211f
Tola Awofolu <oawofolu@pivotal.io>
2023-01-12 00:50:03 -0500

    Added modules

--- rabbitmq/connection/connection_utils.py
+++
@@ -1,7 +0,0 @@
-import logging
-import traceback
-
-
-def exception_handler(args):
-    logging.error(f'caught {args.exc_type} with value {args.exc_value} in thread {args.thread}\n')
-    logging.error(traceback.format_exc())
